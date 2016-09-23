382112a3fa58d946fcc45fb68aebcc1d6d4462f9
Serega <serv25@i.ua>
2016-09-23 16:09:08 +0300

    up

--- app/src/main/java/com/example/serega/mylauncher/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/serega/mylauncher/MyRecyclerAdapter.java
@@ -68,6 +68,10 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
             appName = (TextView) v.findViewById(R.id.app_view);
             appIcon = (ImageView) v.findViewById(R.id.app_icon);
             appCheckBox = (CheckBox) v.findViewById(R.id.on_desktop);
+
+            v.setOnClickListener(onItemClick);
+            v.setOnLongClickListener(onItemLongClick);
+
             appCheckBox.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -89,75 +93,89 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
                     }
                 }
             });
-            v.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    String name = getApps().get(position).getName().toString();
-                    PackageManager manager = v.getContext().getPackageManager();
-                    try {
-                        Intent intent = manager.getLaunchIntentForPackage(name);
-                        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                        v.getContext().startActivity(intent);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            v.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    View inflatedView = view.inflate(view.getContext(), R.layout.popup_window, null);
-                    popupWindow = new PopupWindow(
-                            inflatedView,
-                            ViewGroup.LayoutParams.WRAP_CONTENT,
-                            ViewGroup.LayoutParams.WRAP_CONTENT
-                    );
-                    popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                    popupWindow.setOutsideTouchable(true);
-                    popupWindow.showAsDropDown(view, 0, 0);
-                    TextView uninstall = (TextView) inflatedView.findViewById(R.id.uninstall);
-                    TextView aboutApp = (TextView) inflatedView.findViewById(R.id.about_app);
-                    aboutApp.setOnClickListener(onClickListener);
-                    uninstall.setOnClickListener(onClickListener);
-                    return true;
-                }
-            });
         }
 
-        private View.OnClickListener onClickListener = new View.OnClickListener() {
+        private View.OnClickListener onItemClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchApp(view);
+            }
+        };
+
+        private View.OnLongClickListener onItemLongClick = new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                View inflatedView = view.inflate(view.getContext(), R.layout.popup_window, null);
+                popupWindow = new PopupWindow(
+                        inflatedView,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                );
+                popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                popupWindow.setOutsideTouchable(true);
+                popupWindow.showAsDropDown(view, 0, 0);
+                TextView uninstall = (TextView) inflatedView.findViewById(R.id.uninstall);
+                TextView aboutApp = (TextView) inflatedView.findViewById(R.id.about_app);
+                aboutApp.setOnClickListener(onItemSubMenuClick);
+                uninstall.setOnClickListener(onItemSubMenuClick);
+                return true;
+            }
+        };
+
+        private View.OnClickListener onItemSubMenuClick = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int position = getAdapterPosition();
                 String name = getApps().get(position).getName().toString();
                 switch (view.getId()) {
                     case R.id.uninstall:
-                        Intent intent = new Intent(Intent.ACTION_DELETE);
-                        intent.setData(Uri.parse("package:" + name));
-                        try {
-                            if (getApps().get(position).isOnDesktop()) {
-                                int count = MainActivity.getCurrentAmountOfApps() - 1;
-                                MainActivity.setCurrentAmountOfApps(count);
-                            }
-                            view.getContext().startActivity(intent);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        uninstallApp(view, position,name);
                         break;
                     case R.id.about_app:
-                        Intent i = new Intent();
-                        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                        Uri uri = Uri.fromParts("package", name, null);
-                        i.setData(uri);
-                        try {
-                            view.getContext().startActivity(i);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                        aboutApp(view, name);
                         break;
                 }
                 popupWindow.dismiss();
             }
         };
+
+        private void launchApp(View view){
+            int position = getAdapterPosition();
+            String name = getApps().get(position).getName().toString();
+            PackageManager manager = view.getContext().getPackageManager();
+            try {
+                Intent intent = manager.getLaunchIntentForPackage(name);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                view.getContext().startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void uninstallApp(View view, int position, String name){
+            Intent intent = new Intent(Intent.ACTION_DELETE);
+            intent.setData(Uri.parse("package:" + name));
+            try {
+                if (getApps().get(position).isOnDesktop()) {
+                    int count = MainActivity.getCurrentAmountOfApps() - 1;
+                    MainActivity.setCurrentAmountOfApps(count);
+                }
+                view.getContext().startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void aboutApp(View view, String name){
+            Intent i = new Intent();
+            i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", name, null);
+            i.setData(uri);
+            try {
+                view.getContext().startActivity(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
